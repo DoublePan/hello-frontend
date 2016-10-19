8e3234c023e71b37b7e239e5c2e753bb7610b1ab
DoublePan <517561494@qq.com>
2016-10-19 21:24:53 +0800

    YunGuy.6.0 with Form Validation

--- site/YunGuy/js_30.js
+++ site/YunGuy/js_30.js
@@ -1,53 +1,92 @@
 //global variables area
-var registerForm=document.getElementById("register-form");
-var validationResult=registerForm.getElementsByTagName("div").length; //0 success; >0 fail
+var registerForm = document.getElementById("register-form");
+var submitBtn = document.getElementById("submit-btn");
+var emailReg = /.+@.+\..+/;
 
-window.onload=function() {
-	// useCaputer parementer must be Ture, focus event is not bubbling.
-	registerForm.addEventListener('focus',showTip,true);
-	registerForm.addEventListener('blur',showValidationMsg,true);
-	registerForm.addEventListener('submit',submitValidation,true);
+window.onload = function() {
+    // useCaputer parementer must be Ture, focus event is not bubbling.
+    registerForm.addEventListener('focus', showTip, true);
+    registerForm.addEventListener('blur', showValidationMsg, true);
+    registerForm.addEventListener('submit', submitValidation, true);
 }
 
 
 function showTip() {
-	var targetNode=event.target;
-	var targetTip=targetNode.nextElementSibling;
-	targetTip.style.visibility="visible";
+    var targetNode = event.target;
+    var targetTip = targetNode.nextElementSibling;
+    targetTip.style.visibility = "visible";
 }
 
 
 function showValidationMsg() {
-	var targetNode=event.target;
-	var targetValue=targetNode.value;
-	var targetTip=targetNode.nextElementSibling;
-	// targetTip.style.display="none";
-	if (targetValue.length==0) {
-		targetNode.style.border="1px solid red";
-		targetTip.textContent=targetNode.name+" can't be null!";
-		targetTip.style.color="red";
-	} else {
-		targetNode.style.border="1px solid green";
+    var targetNode = event.target;
+    validateElement(targetNode);
+}
 
-		targetTip.textContent=targetNode.name+" is validated successfullly";
-		targetTip.style.color="green";
-		validationResult--;		
 
-	}
+//validate form element
+function validateElement(targetNode) {
+    var targetValue = targetNode.value;
+    var targetTip = targetNode.nextElementSibling;
+    targetTip.style.visibility = "visible";
+
+    if (targetNode.name == "email") {
+        if (!emailReg.test(targetValue)) {
+            targetNode.style.border = "1px solid red";
+            targetTip.textContent = "Invalid email format";
+            targetTip.style.color = "red";
+            return false;
+        } else {
+            targetNode.style.border = "1px solid green";
+            targetTip.textContent = "Validated successfullly";
+            targetTip.style.color = "green";
+            return true;
+        }
+    }
+
+
+    if (targetNode.name == "poetSpecification") {
+        if (targetValue.length == 0) {
+            targetNode.style.border = "1px solid red";
+            targetTip.textContent = "poetSpecification can't be null";
+            targetTip.style.color = "red";
+            return false;
+        } else {
+            targetNode.style.border = "1px solid green";
+            targetTip.textContent = "Validated successfullly";
+            targetTip.style.color = "green";
+            return true;
+        }
+    }
 
 
 }
+
 
 
 function submitValidation() {
-	// if(validationResult==0) {
-	// alert("validation success!");
-	// console.log("success: "+registerForm.email.value);
+    submitBtn.disabled = true;
 
-	// } else {
-	// alert("validation failed!");
 
-	// }
+    var validateFailCnt = 0;
+    try {
+        for (var ele of registerForm.elements) {
+            if (!validateElement(ele)) {
+                validateFailCnt++;
+            }
+
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+
+
+    console.log(validateFailCnt);
+    if (validateFailCnt != 0) {
+        event.preventDefault();
+    }
+    // return false;
+
+    submitBtn.disabled = false;
+
 }
-
-
